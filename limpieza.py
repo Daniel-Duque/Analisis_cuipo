54d19fd1f6726fc64ecf1bc58b7476503fb2301b
Daniel-Duque <daniel_duque@javeriana.edu.co>
2024-02-27 16:13:45 -0500

    Optimizaction

--- limpieza.py
+++ limpieza.py
@@ -15,27 +15,31 @@ import plotly.express as px
 
 
 
+eje_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Gastos_20240213.csv",usecols=["NOMBRE_ENTIDAD"])
 
 
 import streamlit as st
 
 st.set_page_config(layout='wide')
 
-
-filtrado1=pd.read_csv(
-    r"bases_cuipo_contraloria/nombre_municipios.csv")
-print(filtrado1["NOMBRE_ENTIDAD"])
-
+municipio=eje_gasto["NOMBRE_ENTIDAD"].drop_duplicates()
 st.title("gasto presupuestal")
 
 tab0,tab1,tab2 = st.tabs(['selección','gastos',"nombres"])
 with tab0:
   muni = st.selectbox("NOMBRE_ENTIDAD",
-                        filtrado1["NOMBRE_ENTIDAD"])
-                          
+                        municipio)
+                        
+              
+  filter_muni=eje_gasto[
+  eje_gasto["NOMBRE_ENTIDAD"]== muni]
+  filtrado1=filter_muni.index.values
+
+  
+  
   #pres_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Programaci_n_de_Gastos_20240213.csv",skiprows=lambda x: x not in filter_muni)
   #pres_ingr=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Programaci_n_de_Ingresos_20240213.csv",skiprows=lambda x: x not in filter_muni)
-  
+  eje_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Gastos_20240213.csv",skiprows=lambda x: x not in filtrado1)
   #eje_ingr=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Ingresos_20240213.csv",skiprows=lambda x: x not in filter_muni)
   
   print("first stage")
@@ -43,10 +47,9 @@ with tab0:
   
 with tab1:
 
-  eje_gasto=pd.read_csv(
-    r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Gastos_20240213.csv",skiprows=lambda x: x not in filtrado1)
-    
-  cuenta3=  pd.read_csv(r"bases_cuipo_contraloria/nombre_municipios")
+  cuenta3=eje_gasto[
+	"NOMBRE_CUENTA"].drop_duplicates()
+
   category = st.selectbox("NOMBRE_CUENTA",
                         cuenta3)
   filter_category = eje_gasto[eje_gasto["NOMBRE_CUENTA"]== category]
