cd891ef7a02c6320689cc478b1a444d28c92b25b
Daniel-Duque <daniel_duque@javeriana.edu.co>
2024-02-29 14:32:58 -0500

    updated particular codes per municipalities
    :D

--- limpieza.py
+++ limpieza.py
@@ -8,55 +8,30 @@ Original file is located at
 """
 
 
-import streamlit
 import pandas as pd
 import os
-import plotly.express as px
-
-
-
-eje_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Gastos_20240213.csv",usecols=["NOMBRE_ENTIDAD"])
-
-
-import streamlit as st
-
-st.set_page_config(layout='wide')
-
-municipio=eje_gasto["NOMBRE_ENTIDAD"].drop_duplicates()
-st.title("gasto presupuestal")
-
-tab0,tab1,tab2 = st.tabs(['selección','gastos',"nombres"])
-with tab0:
-  muni = st.selectbox("NOMBRE_ENTIDAD",
-                        municipio)
-                        
-              
-  filter_muni=eje_gasto[
-  eje_gasto["NOMBRE_ENTIDAD"]== muni]
-  filtrado1=filter_muni.index.values
 
   
   
-  #pres_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Programaci_n_de_Gastos_20240213.csv",skiprows=lambda x: x not in filter_muni)
-  #pres_ingr=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Programaci_n_de_Ingresos_20240213.csv",skiprows=lambda x: x not in filter_muni)
-  eje_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Gastos_20240213.csv",skiprows=lambda x: x not in filtrado1)
-  #eje_ingr=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Ingresos_20240213.csv",skiprows=lambda x: x not in filter_muni)
-  
-  print("first stage")
-  
-  
-with tab1:
+pres_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Programaci_n_de_Gastos_20240213.csv")
+pres_ingr=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Programaci_n_de_Ingresos_20240213.csv")
+eje_gasto=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Gastos_20240213.csv")
+eje_ingr=pd.read_csv(r"bases_cuipo_contraloria/OVCF_-_CUIPO_-_Ejecuci_n_de_Ingresos_20240213.csv")
 
-  cuenta3=eje_gasto[
-	"NOMBRE_CUENTA"].drop_duplicates()
+full=pd.concat([pres_gasto,pres_ingr], ignore_index=True)
+full=pd.concat([full,eje_ingr], ignore_index=True)
+full=pd.concat([full,eje_gasto], ignore_index=True)
 
-  category = st.selectbox("NOMBRE_CUENTA",
-                        cuenta3)
-  filter_category = eje_gasto[eje_gasto["NOMBRE_CUENTA"]== category]
-  
+pres_gasto=0
+pres_ingr=0
+eje_gasto=0
+eje_ingr=0
 
+names=full[['CODIGO_ENTIDAD',"NOMBRE_ENTIDAD"]].drop_duplicates(subset=['CODIGO_ENTIDAD'])
+names.to_csv(r"dic/codigos_nombres")
 
-
-  fig = px.line(filter_category, x="VIGENCIA", y='PAGOS', title='APROPIACION_INICIAL')
-  st.plotly_chart(fig)
+for codigo in names["CODIGO_ENTIDAD"]:
+    print(codigo)
+    full[full["CODIGO_ENTIDAD"]==codigo].to_csv(
+        r"bases_cuipo_contraloria/municipalities/"+str(codigo)+".csv")
 
